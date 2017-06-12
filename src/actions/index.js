ecb6f71ffe0c09659125dc5dbe860c876aed40e4
mfpinheiro <mateusfpinheiro@gmail.com>
2017-06-11 23:24:45 -0300

    Add post function with a action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POSTS = 'CREATE_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
@@ -12,4 +13,13 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   }
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+  return {
+    type: CREATE_POSTS,
+    payload: request
+  }
+
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,30 +3,36 @@
  */
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostNew extends Component {
 
   renderField = (field) => {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ' '}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label htmlFor={field.name}>{field.label}</label>
         <input
           name={field.name}
           className="form-control"
           type="text"
           {...field.input} />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   };
 
   onSubmit = (values) => {
-    console.log(values)
+    this.props.createPost(values);
   };
 
   render() {
     const { handleSubmit } = this.props;
-
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -46,6 +52,7 @@ class PostNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -70,4 +77,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostNewForm'
-})(PostNew);
+})(
+  connect(null, {createPost})(PostNew)
+);
