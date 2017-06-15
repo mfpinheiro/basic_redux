2e5bb4789c2b25243184e8b2a639510b08096754
mfpinheiro <mateusfpinheiro@gmail.com>
2017-06-15 10:24:37 -0300

    Add fetch_post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POSTS = 'CREATE_POSTS';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
@@ -22,5 +23,13 @@ export function createPost(values, callback) {
     type: CREATE_POSTS,
     payload: request
   }
+}
 
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,10 +3,18 @@
  */
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
-
+import { FETCH_POST } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      const post = action.payload.data;
+      // ES5 style
+      // const newState =  { ...state };
+      // newState[post.id] = post;
+      // return newState;
+      //  ES6 style - Make a new key and set a new data to it
+      return { ...state, [action.payload.data.id] : action.payload.data }
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
